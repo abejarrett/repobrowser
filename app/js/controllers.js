82c932b2a357f3e2f0abf221420ecec41929b176
Abe Jarrett <ajarrett@Abes-MBP.att.net>
2016-02-08 15:59:47 -0800

    added paging support for repos of an org

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,14 +1,42 @@
 
 
 angular.module('GHRepoBrowserApp.controllers',[]).
+
+filter('startFrom',function(){
+   return function(data, start){
+    return data.slice(start);
+ }
+}).
   controller('repoController', function($scope, ghAPIservice) {
 
     $scope.repoList = [];
+    $scope.linkheaders;
     $scope.error = false;
     $scope.commitserror = false;
     $scope.commits = [];
     $scope.showcommits = false;
 
+    //break down the link header
+    function parse_link_header(header) {
+      if (header.length === 0) {
+          throw new Error("input must not be of zero length");
+      }
+
+      // Split parts by comma
+      var parts = header.split(',');
+      var links = {};
+      // Parse each part into a named link
+      for(var i=0; i<parts.length; i++) {
+          var section = parts[i].split(';');
+          if (section.length !== 2) {
+              throw new Error("section could not be split on ';'");
+          }
+          var url = section[0].replace(/<(.*)>/, '$1').trim();
+          var name = section[1].replace(/rel="(.*)"/, '$1').trim();
+          links[name] = url;
+      }
+      return links;
+    }
 
     $scope.$watch(function () { return ghAPIservice.getOrgName(); }, function (newValue, oldValue) {
         if (newValue !== oldValue) {
@@ -16,10 +44,33 @@ angular.module('GHRepoBrowserApp.controllers',[]).
         }
     });
 
+    $scope.nextPage = function () {
+      var url = $scope.linkheaders.next;
+      getorgrepos(url);
+      console.log(url);
+    }
+
+    $scope.prevPage = function () {
+      var url = $scope.linkheaders.prev;
+      getorgrepos(url);
+      console.log("clicked prev page");
+    }
+
+    $scope.lastPage = function () {
+      var url = $scope.linkheaders.next;
+      getorgrepos(url);
+      console.log (url);
+    }
+
  //get the name once enter key is pressed
-     $scope.action = function () {
-          ghAPIservice.getOrgRepos().success(function (response) {
-               $scope.repoList = response;
+     $scope.action = function (url) {
+          getorgrepos(url);
+    };
+
+    function getorgrepos (url) {
+          ghAPIservice.getOrgRepos(url).success(function (result, status, headers, config) {
+               $scope.repoList = result;
+               $scope.linkheaders = parse_link_header(headers("link"));
                $scope.error= false;
             }).error(function(response) {
                 $scope.error = true;
--- app/js/services.js
+++ app/js/services.js
@@ -13,17 +13,16 @@ angular.module('GHRepoBrowserApp.services', []).
             this.OrgName = orgName;
     }
 
-
-
-
-    ghAPI.getOrgRepos = function() {
-        var _url = 'https://api.github.com/orgs/' + ghAPI.getOrgName() + '/repos';
+    //kick off main repo by org request
+    ghAPI.getOrgRepos = function(url) {
+        var _url = url ? url :'https://api.github.com/orgs/' + ghAPI.getOrgName() + '/repos';
         return $http({
           method: 'GET',
           url: _url
         });
     }
 
+    //kick of request for commits by repo
     ghAPI.getRepoCommits = function(repo){
         var _url = "https://api.github.com/repositories/" + repo.id + "/commits";
         return $http({
